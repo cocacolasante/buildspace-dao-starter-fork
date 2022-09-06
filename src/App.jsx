82ab70eb95ccee7c15d5d643afa06f24273a7c8c
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-09-05 20:25:31 -0400

    connect to metamask button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { useAddress, useMetamask } from "@thirdweb-dev/react";
+
+
 const App = () => {
+
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+
+  console.log("Connected to: ", address);
+
+  if(!address){
+    return (
+      <div className="landing">
+      <h1>Welcome to Coders Dao</h1>
+      <p>A Place for Blockchain Coders to work together to build the new internet</p>
+      <button onClick={connectWithMetamask} >Please connect your wallet</button>
+    </div>
+    )
+  }
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Welcome to Coders Dao</h1>
+      <p>A Place for Blockchain Coders to work together to build the new internet</p>
+      <p>Wallet connected. Now What????</p>
     </div>
   );
 };
